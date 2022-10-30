009a35cb2fe7f7daff237c7512071d61ecce5f3c
MODZERS <116981625+MODZERS@users.noreply.github.com>
2022-10-29 23:45:50 -0400

    Add files via upload

---
+++ hacker typer/README.txt
@@ -0,0 +1,2 @@
+if this programe all the stuff you type will be in green and look like you are a hacker
+bo ha ha ha bo ha ha ha.
---
+++ hacker typer/hacker type.bat
@@ -0,0 +1,19 @@
+@echo off
+
+    (
+        title HACKER TYPE
+        color 2
+    )
+
+            :type
+            
+(
+    set /p type=
+)
+
+
+goto type
+
+(
+    if %type% == 12312321478462387432678436287432678 exit
+)
